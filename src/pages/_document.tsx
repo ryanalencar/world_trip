37e3268f3ff3ca1e0b4de5f8eec70986375eb18d
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-06-28 20:22:44 -0300

    feat: poppins font setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,14 @@ export default class Document extends NextDocument {
       <Html lang="pt-br">
         <Head>
           <title>World Trip</title>
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
