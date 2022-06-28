ec9006dd296e735146dbe80d16b2fb1eb362139d
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-06-28 19:43:07 -0300

    feat: document's next

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,17 @@
-import NextDocument, { Html, Head } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="pt-br">
-        <Head />
+        <Head>
+          <title>World Trip</title>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     );
   }
