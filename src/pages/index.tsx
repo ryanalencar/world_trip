4df14c49b7058a0c50b6c63b7ad166ee0ebe108c
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-06-29 20:28:08 -0300

    feat: section divider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Center, Divider } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 import Banner from '../components/Banner';
@@ -9,6 +10,15 @@ const Home: NextPage = () => (
     <Header />
     <Banner />
     <TravelTypes />
+    <Center w="100%" my={20}>
+      <Divider
+        w="90px"
+        orientation="horizontal"
+        borderRadius={5}
+        borderWidth={2}
+        borderColor="dark.heading"
+      />
+    </Center>
   </>
 );
 
